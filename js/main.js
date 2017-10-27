566be2beab05237ca0bc2e0a777c923e57fb3fcc
grezik <grzegorz.mazik.jr@gmail.com>
2017-10-27 10:21:14 +0200

    mousedown and mouseup events - first version

--- js/main.js
+++ js/main.js
@@ -15,19 +15,18 @@ document.addEventListener('DOMContentLoaded', function () {
 	function removeTransition(e) {
 		if (e.propertyName !== 'box-shadow' && e.propertyName !== 'background-image') return;
 		this.classList.remove('playing');
-		// console.log(e);
+		console.log(e);
 	}
 
 
-	const pads = document.querySelectorAll('.pad');
-	pads.forEach(pad => pad.addEventListener('transitionend', removeTransition));
-
 	window.addEventListener('keydown', playSound);
+	
+	const pads = document.querySelectorAll('.pad');
+	// pads.forEach(pad => pad.addEventListener('transitionend', removeTransition));
 
 	// Unobstrusive event binding
-	pads.forEach(pad => pad.addEventListener('click', function (e) {
+	pads.forEach(pad => pad.addEventListener('mousedown', function (e) {
 		// store data-key value, a js keyCode paired with a pad to trigger corresponding sound file
-
 		const keyValue = this.dataset.key;
 
 		// select audio file to play based on keyValue, ie. pair an audio tag with a corresponding .pad div
@@ -38,6 +37,20 @@ document.addEventListener('DOMContentLoaded', function () {
 		sound.currentTime = 0;
 		sound.play();
 		this.classList.add('playing');
+		console.log(e);
+	}));
+	pads.forEach(pad => pad.addEventListener("mouseup", function (e) {
+		this.classList.remove('playing');
+		console.log("e: " + e);
+		console.log("this: " + this)
 	}));
 
 });
+
+
+/*
+ * stworzyc funkcje, ktora bedzie ustawiac klase playing
+ * i przekazac jej w parametrze obiekt, w ktorym ma ustawic klase.
+ * w funkcji dla eventdown - dodać klasę, w funkcji dla eventup - usunąć klasę
+ * tu może transitionend event nie jest potrzebny?
+ */
